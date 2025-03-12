55f98ec0867a19e7c0d6c3ed92a7406e4288879d
Jaspe Michael <micaeljaspe@gmail.com>
2025-03-12 19:50:42 +0200

    migrated to Pckr

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,160 +1,164 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
+local function bootstrap_pckr()
+  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+
+  if not (vim.uv or vim.loop).fs_stat(pckr_path) then
+    vim.fn.system({
+      'git',
+      'clone',
+      "--filter=blob:none",
+      'https://github.com/lewis6991/pckr.nvim',
+      pckr_path
+    })
   end
-  return false
+
+  vim.opt.rtp:prepend(pckr_path)
 end
 
-local packer_bootstrap = ensure_packer()
+bootstrap_pckr()
 
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use "sainnhe/sonokai"
-	use "nvim-tree/nvim-tree.lua"
-	use "nvim-tree/nvim-web-devicons"
-	use "terrortylor/nvim-comment"
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/nvim-lsp-installer",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	}
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'L3MON4D3/LuaSnip'
-	use "rafamadriz/friendly-snippets"
-	use({
-		"NTBBloodbath/galaxyline.nvim",
-		-- your statusline
-		-- config = function()
-		-- require("galaxyline.themes.eviline")
-		-- end,
-		-- some optional icons
-		requires = { "kyazdani42/nvim-web-devicons", opt = true }
-	})
-	use 'rose-pine/neovim'
-	use {
-		'romgrk/barbar.nvim',
-		requires = {'lewis6991/gitsigns.nvim'}
-	}
-	use {'nyoom-engineering/oxocarbon.nvim'}
-	use { 'nguyenvukhang/nvim-toggler' }
-	use {
-		"akinsho/toggleterm.nvim",
-		tag = '*',
-		}
-	use {
-		"petertriho/nvim-scrollbar",
-		config = function()
-		require("scrollbar").setup()
-		end
-	}
-	use { "RRethy/vim-illuminate" }
-	use {
+require('pckr').add{
+	-- Icons plugins
+	"lewis6991/gitsigns.nvim";
+	"kyazdani42/nvim-web-devicons";
+	"nvim-tree/nvim-web-devicons";
+	{
 		'yamatsum/nvim-nonicons',
 		requires = {'kyazdani42/nvim-web-devicons'},
 		config = function()
-		require("nvim-nonicons").setup{}
+			require("nvim-nonicons").setup{}
 		end
-	}
+	};
 
-	use {
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate"
-	}
-	use {
+	-- The famous telescope
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	};
+	"rcarriga/nvim-notify";
+
+	-- LSP plugins
+	"williamboman/mason.nvim";
+	"williamboman/nvim-lsp-installer";
+	"williamboman/mason-lspconfig.nvim";
+	"neovim/nvim-lspconfig";
+	"winston0410/cmd-parser.nvim";
+	"hrsh7th/nvim-cmp";
+	"hrsh7th/cmp-nvim-lsp";
+
+	-- Colorschemes and related
+	"sontungexpt/witch";
+	"sainnhe/sonokai";
+	"Mofiqul/adwaita.nvim";
+	"kyazdani42/blue-moon";
+	"rafamadriz/neon";
+	"fenetikm/falcon";
+	"rose-pine/neovim";
+	"zaldih/themery.nvim";
+	"xiyaowong/transparent.nvim";
+	"mhartington/oceanic-next";
+	'nyoom-engineering/oxocarbon.nvim';
+	{
+		"tiagovla/tokyodark.nvim",
+		config = function()
+			require("tokyodark").setup()
+		end,
+	};
+	{
+		'romgrk/barbar.nvim',
+		requires = {'lewis6991/gitsigns.nvim'}
+	};
+	{
 		'glepnir/zephyr-nvim',
 		requires = { 'nvim-treesitter/nvim-treesitter', opt = true },
-	}
-	use "yamatsum/nvim-cursorline"
-	use { 'michaelb/sniprun', run = 'sh ./install.sh'}
-	use "ellisonleao/dotenv.nvim"
-	use {
+	};
+	"ellisonleao/gruvbox.nvim";
+	"ellisonleao/glow.nvim";
+
+	-- Tree file plugin
+	"nvim-tree/nvim-tree.lua";
+	{
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate"
+	};
+	{
+		"ramojus/mellifluous.nvim",
+		config = function()
+			require("mellifluous").setup({})
+		end
+	};
+	{
+		"startup-nvim/startup.nvim",
+		requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"}
+	};
+
+	-- Comment plugin
+	"terrortylor/nvim-comment";
+
+	-- Snipets and code-related
+	"L3MON4D3/LuaSnip";
+	"rafamadriz/friendly-snippets";
+	"ellisonleao/dotenv.nvim";
+	{'michaelb/sniprun', run = 'sh ./install.sh'};
+
+	-- Draw
+	"jbyuki/venn.nvim";
+
+	-- Interface plugins
+	"RRethy/vim-illuminate";
+	{
+		"petertriho/nvim-scrollbar",
+		config = function()
+			require("scrollbar").setup()
+		end
+	};
+	"yamatsum/nvim-cursorline";
+	"preservim/tagbar";
+	"ofirgall/ofirkai.nvim";
+	{
+		"NTBBloodbath/galaxyline.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true }
+	};
+	{
 		"folke/twilight.nvim",
 		config = function()
 			require("twilight").setup()
 		end
-	}
-	use({
+	};
+
+	-- Others
+
+	{
 		"kdheepak/lazygit.nvim",
 		requires = {
 			"nvim-lua/plenary.nvim",
 		},
-	})
-	use {
+	};
+
+	'nguyenvukhang/nvim-toggler';
+	{
+		"akinsho/toggleterm.nvim",
+		tag = '*'
+	};
+
+	{
 		'2kabhishek/nerdy.nvim',
 		config = function()
 			require('telescope').load_extension('nerdy')
 		end,
-	}
-	use 'zaldih/themery.nvim'
-	use { "ellisonleao/gruvbox.nvim" }
-	use {"ellisonleao/glow.nvim"}
-	use {
+	};
+	{
 		"OXY2DEV/markview.nvim",
-		dependencies = {
+		requires = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-tree/nvim-web-devicons"},
-	}
-	use {
-    "tiagovla/tokyodark.nvim",
-    config = function()
-        require("tokyodark").setup()
-    end,
-	}
-	use 'Mofiqul/adwaita.nvim'
-	use({
-		"ramojus/mellifluous.nvim",
-		config = function()
-			require("mellifluous").setup({})
-		end,
-	})
-	use 'xiyaowong/transparent.nvim'
-	use 'mhartington/oceanic-next'
-	-- packer.nvim
-	use({
-		"robitx/gp.nvim",
-	})
-	use {
-	  'jghauser/follow-md-links.nvim'
-	}
-	use {
-		"startup-nvim/startup.nvim",
-		requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"}
-	}
-	use {
-		"sontungexpt/witch"
-	}
-	use "preservim/tagbar"
-	use "kyazdani42/blue-moon"
-	use "rafamadriz/neon"
-	use 'ofirgall/ofirkai.nvim'
-	use "fenetikm/falcon"
-	use "jbyuki/venn.nvim"
-	use {
+	};
+	{
 		"epwalsh/pomo.nvim",
 		tag = "*",  -- Recommended, use latest release instead of latest commit
 		requires = {"rcarriga/nvim-notify"}
-	}
-	use "tamton-aquib/mpv.nvim"
-	use { 'codota/tabnine-nvim', run = "./dl_binaries.sh" }
-	use {
+	};
+	{
 		"winston0410/range-highlight.nvim",
-		requires = {'winston0410/cmd-parser.nvim'},
-	}
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end
-)
+		requires = {'winston0410/cmd-parser.nvim'}
+	};
+}
